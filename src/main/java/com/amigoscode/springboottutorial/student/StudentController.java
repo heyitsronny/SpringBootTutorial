5b15714dae9daa946a35bc2c2614dbdb68dbcc64
Ronald Baumgarten <ronald.baumgarten@informatik.hu-berlin.de>
2021-11-16 14:20:30 +0100

    Add Delete Mapping for deleting a student

--- src/main/java/com/amigoscode/springboottutorial/student/StudentController.java
+++ src/main/java/com/amigoscode/springboottutorial/student/StudentController.java
@@ -3,11 +3,7 @@ package com.amigoscode.springboottutorial.student;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
@@ -28,5 +24,10 @@ public class StudentController {
     @PostMapping
     public void registerNewStudent(@RequestBody Student student) {
         studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "/{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
     }
 }
